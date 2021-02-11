2ca330e59685777ae9de4d2a8743da52c8b5ecbe
Dave Powell <evadllewop@gmail.com>
2021-02-11 16:12:42 -0600

    adding routes and models

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const logger = ("morgan");
+const logger = require("morgan");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 require("./routes/apiRoutes.js")(app);
 require("./routes/htmlRoutes.js")(app);
