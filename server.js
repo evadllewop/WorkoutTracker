2e7f19a43042f9125a3ea4c68aadcd0458ea61ca
Dave Powell <evadllewop@gmail.com>
2021-02-12 12:28:07 -0600

    deploying

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 
@@ -11,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 require("./routes/apiRoutes.js")(app);
 require("./routes/htmlRoutes.js")(app);
